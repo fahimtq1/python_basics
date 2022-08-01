f3e9dc458ed893cc76e09a40e8a169d91a972d0e
fahimtq1 <fahimqurashi123@outlook.com>
2022-08-01 14:03:22 +0100

    Added new operator files with new documentation

---
+++ README.md
@@ -0,0 +1,33 @@
+# Python introduction
+
+## Python variables
+
+What are variables?
+- The shorthand for variables is var
+- They store data of different types
+
+Types of variables:
+- int
+- float
+- boolean
+- string
+
+## Operators
+
+There are two types of operators. 
+
+### Arithmetic operators:
+- ```+``` 
+- ```-```
+- ```*```
+- ```/```
+
+### Comparison operators:
+- `<`
+- `>`
+- `==`
+- `!=`
+- `<=`
+- `>=`
+
+
---
+++ python_variable.py
@@ -0,0 +1,48 @@
+# Python variables tutorial
+
+print("Hello World")   # testing PyCharm
+
+'''
+What are variables?
+- The shorthand for variables is var
+- They store data of different types
+
+Types of variables:
+- int
+- float
+- boolean
+- string
+'''
+
+# Example variables with input()
+print("Welcome!")
+
+print("Please enter your first name")
+first_name = input()
+print(first_name)
+print(type(first_name))
+
+print("Please enter your last name")
+last_name = input()
+print(last_name)
+print(type(last_name))
+
+print("Please enter your age")
+age = int(input())
+print(age)
+print(type(age))
+
+print("Please enter your post code")
+post_code = input()
+print(post_code)
+print(type(post_code))
+
+print("Please enter your course")
+course = input()
+print(course)
+print(type(course))
+
+print("Please enter your salary")
+salary = float(input())
+print(salary)
+print(type(salary))
