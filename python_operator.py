1c7c884d199328991a0691ae6fed13ee5d2c97da
fahimtq1 <fahimqurashi123@outlook.com>
2022-08-01 14:39:20 +0100

    changes made to python_operator.py

--- python_operator.py
+++ python_operator.py
@@ -1,2 +1,27 @@
 # Python operators tutorial
 
+# functions method builtin python
+greeting = "Hello World"
+# what options are available in python's builtin library
+print(greeting)
+# # if we want to check whether there are letters in the string
+# print(greeting.isalpha())   # .isalpha() checks whether there are only letters in this string variable
+# # is it in lower case or upper case
+# # print(greeting.islower())
+# # print(greeting.isupper())
+# # print(greeting.isdigit())
+# # print(greeting.startswith("H"))
+# # print(greeting.endswith("d"))
+# #
+# # # # Indexing
+# # # print(len(greeting))
+# # # print(greeting[-5])   # W
+# # # print(greeting[5])   #
+# # # print(greeting[:5])   # Hello
+# # # print(greeting[5:])
+
+# Indexing and slicing exercise
+print(greeting[6:])
+print(greeting[3])
+print(greeting[:7])
+print(greeting[:6])
