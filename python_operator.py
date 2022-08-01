da192f42ae182b1686597f3d6e25e5fe00e9aa55
fahimtq1 <fahimqurashi123@outlook.com>
2022-08-01 14:56:57 +0100

    Final commit before the break

--- python_operator.py
+++ python_operator.py
@@ -1,9 +1,9 @@
 # Python operators tutorial
 
 # functions method builtin python
-greeting = "Hello World"
-# what options are available in python's builtin library
-print(greeting)
+# greeting = "Hello World"
+# # what options are available in python's builtin library
+# print(greeting)
 # # if we want to check whether there are letters in the string
 # print(greeting.isalpha())   # .isalpha() checks whether there are only letters in this string variable
 # # is it in lower case or upper case
@@ -21,7 +21,15 @@ print(greeting)
 # # # print(greeting[5:])
 
 # Indexing and slicing exercise
-print(greeting[6:])
-print(greeting[3])
-print(greeting[:7])
-print(greeting[:6])
+# print(greeting[6:])
+# print(greeting[3])
+# print(greeting[:7])
+# print(greeting[:6])
+
+random_text = "Lot's of text but why do I text so much"
+print(random_text)
+print(random_text.upper())
+print(random_text.capitalize())   # this makes the first letter of the string a capital letter
+print(random_text.lower())
+print(random_text.count("text"))
+print((random_text.replace("Lot's","Lots")))
