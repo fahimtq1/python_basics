12e3870c5c39f3bc9ae12eb4bf3ca498cb401d2b
fahimtq1 <fahimqurashi123@outlook.com>
2022-08-03 13:58:20 +0100

    lol

--- python_function.py
+++ python_function.py
@@ -6,7 +6,25 @@ def greet_user():
     full_name = ("%s %s" % (first_name.capitalize(), last_name.capitalize()))
     return print(f"Welcome {full_name}!")
 
-# Create a function that takes two arguments and adds them
-def add(a,b):
-    return print(a + b)
+# Create a basic calculator
+
+class calc:
+    def add(a, b):
+        '''Function that adds the inputs'''
+        return a + b
+
+    def subtract(a, b):
+        '''Function that divides the inputs'''
+        return a - b
+
+    def multiply(a, b):
+        '''Function that multiplies the inputs'''
+        return a * b
+
+    def divide(a, b):
+        '''Function that divides the inputs'''
+        return a / b
+
+
+
 
